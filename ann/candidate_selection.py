0d78539ae8156310270b265ab7f374a0d0ff8583
alex <alxbar@gmx.de>
2017-07-05 13:46:53 +0200

    added context-and-answer relevance

--- ann/candidate_selection.py
+++ ann/candidate_selection.py
@@ -37,3 +37,28 @@ def answer_relevance(search_context):
         scored.append((score, label))
 
     return scored
+
+def context_and_answer_relevance(search_context):
+    best_n = []
+    for distance, label in zip(search_context['distances'][0], search_context['labels']):
+        utt_emb = search_context['utterance_embeddings']
+        if label[1] >= len(utt_emb[label[0]]):
+            continue
+
+        best_n.append((distance, label, utt_emb[label[0]][label[1]]))
+
+    instances = [(label, emb) for distance, label, emb in best_n]
+    best_n = sorted(best_n, key=lambda pair: pair[0], reverse=False)[0:15]
+
+    final = []
+    for label, emb_cand in instances:
+
+        score = 0
+
+        for distance, label_best_n, emb_best in best_n:
+            score += cosine_similarity(emb_best, emb_cand)
+
+        final.append((score, label))
+    final = sorted(final, key=lambda pair: pair[0])
+
+    return final
